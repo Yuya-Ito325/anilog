627216248ee3b414c577e6fa879437860179b16c
Yuya-Ito325 <c1004072@planet.kanazawa-it.ac.jp>
2021-02-28 17:41:05 +0900

    入力した文字をボタンで表示

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello");
+"use static";
+
+const addButton = document.getElementById('add-button');
+const addName = document.getElementById('add-name');
+const nameList = document.getElementById('name-list');
+const add = document.createElement('li');
+
+const anime = ['keionn','haruhi','syutainnzuge-to']
+
+
+
+
+
+
+addButton.addEventListener('click',function(event){
+if(addName === ''){
+ alert ('文字を入力してください');
+ return;
+}else{
+    const search = addName.value;
+    anime.push(search);
+    add.textContent = anime[anime.length -1];
+    nameList.appendChild(add);
+}
+});
