711058450b0f40f411203d0f00c9a8173e58eda7
Yuya-Ito325 <c1004072@planet.kanazawa-it.ac.jp>
2021-03-04 09:52:29 +0900

    削除ボタン追加

--- index.js
+++ index.js
@@ -20,5 +20,16 @@ if(addName === ''|| addName === null){
     anime.push(search);
     add.textContent = anime[anime.length -1];
     nameList.appendChild(add);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = '削除';
+    deleteButton.addEventListener('click', () => RemoveTask(deleteButton));
+    add.appendChild(deleteButton);
+
 }
 });
+
+const RemoveTask = deleteButton =>{
+    const targetTask = deleteButton.closest('li');
+    nameList.removeChild(targetTask);
+}
